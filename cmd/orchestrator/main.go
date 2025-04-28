5f31a9d418ec796e8a8e4574b50bf357d2e4d863
egocentri <d.kravchenko@naash.ru>
2025-04-28 16:38:42 +0300

    Update main.go

--- cmd/orchestrator/main.go
+++ cmd/orchestrator/main.go
@@ -2,10 +2,7 @@ package main
 
 import (
     "log"
-    "net"
     "os"
-
-    "github.com/egocentri/finalproject/internal/grpc"
     "github.com/egocentri/finalproject/internal/handlers"
     "github.com/egocentri/finalproject/internal/middleware"
     "github.com/egocentri/finalproject/internal/models"
@@ -13,52 +10,43 @@ import (
     jwt "github.com/golang-jwt/jwt/v4"
     "gorm.io/driver/sqlite"
     "gorm.io/gorm"
-    "google.golang.org/grpc"
 )
 
 var jwtSecret = []byte("supersecretkey") 
 func main() {
     db, err := gorm.Open(sqlite.Open("data.db"), &gorm.Config{})
     if err != nil {
-        log.Fatalf("failed to connect database: %v", err)
+        log.Fatalf("DB connect error: %v", err)
     }
     if err := db.AutoMigrate(&models.User{}, &models.Expression{}); err != nil {
-        log.Fatalf("migration failed: %v", err)
+        log.Fatalf("DB migrate error: %v", err)
     }
-
     r := gin.Default()
     authH := handlers.NewAuthHandler(db, jwtSecret)
     exprH := handlers.NewExpressionsHandler(db)
+    taskH := handlers.NewTasksHandler(db)
 
     api := r.Group("/api/v1")
-    api.POST("/register", authH.Register)
-    api.POST("/login", authH.Login)
+    {
+        api.POST("/register", authH.Register)
+        api.POST("/login",    authH.Login)
+    }
     apiAuth := api.Group("/")
     apiAuth.Use(middleware.JWTAuthMiddleware(jwtSecret))
     {
-        apiAuth.POST("/calculate", exprH.Calculate)
-        apiAuth.GET("/expressions", exprH.List)
-        apiAuth.GET("/expressions/:id", exprH.GetByID)
+        apiAuth.POST("/calculate",   exprH.Calculate)
+        apiAuth.GET ("/expressions", exprH.List)
+        apiAuth.GET ("/expressions/:id", exprH.GetByID)
+        // внутренние эндпоинты для агента
+        apiAuth.GET ("/internal/task",       taskH.GetTask)
+        apiAuth.POST("/internal/task",       taskH.PostResult)
     }
-
-    go func() {
-        addr := ":8080"
-        if p := os.Getenv("PORT"); p != "" {
-            addr = ":" + p
-        }
-        log.Printf("HTTP server on %s", addr)
-        if err := r.Run(addr); err != nil {
-            log.Fatalf("HTTP run error: %v", err)
-        }
-    }()
-    lis, err := net.Listen("tcp", ":50051")
-    if err != nil {
-        log.Fatalf("failed to listen: %v", err)
+    port := "8080"
+    if p := os.Getenv("PORT"); p != "" {
+        port = p
     }
-    grpcServer := grpc.NewServer()
-    grpc.RegisterDispatcherServer(grpcServer, grpc.NewServerImpl(db))
-    log.Println("gRPC server on :50051")
-    if err := grpcServer.Serve(lis); err != nil {
-        log.Fatalf("gRPC serve error: %v", err)
+    log.Printf("HTTP server on :%s", port)
+    if err := r.Run(":" + port); err != nil {
+        log.Fatalf("Server error: %v", err)
     }
 }
